d145e07d84a47ad9ab938f2b147541d72205f2bb
Brian Purkiss <brian@themodstudio.com>
2017-10-11 11:41:43 -0500

    Added floats to the card
    plus documentation

--- functions/card.php
+++ functions/card.php
@@ -1,18 +1,21 @@
 <?php
 
+// For Boostrap's Cards
+// https://v4-alpha.getbootstrap.com/components/card/
 
 function bootstrap_card( $atts, $content = null ) {
 	extract( shortcode_atts( array(
 		'width' => '300px',
 		'img' => '',
 		'title' => '',
-		'alt' => ''
+		'alt' => '',
+		'float' => 'right'
 		), $atts )
 	);
 
 	if ( empty($title) ) {
 		return '
-			<div class="card" style="width:' . esc_attr($width) . '">
+			<div class="card" style="width:' . esc_attr($width) . ';float:' . esc_attr($float) . '">
 				<img class="card-img-top" src="' . esc_attr($img) . '" alt="' . esc_attr($alt) . '">
 				<div class="card-block">
 					<p class="card-text">' . esc_attr($content) . '</p>
@@ -21,7 +24,7 @@ function bootstrap_card( $atts, $content = null ) {
 		';
 	} else {
 		return '
-			<div class="card" style="width:' . esc_attr($width) . '">
+			<div class="card" style="width:' . esc_attr($width) . ';float:' . esc_attr($float) . '">
 				<img class="card-img-top" src="' . esc_attr($img) . '" alt="' . esc_attr($alt) . '">
 				<div class="card-block">
 					<h5 class="card-title">' . esc_attr($title) . '</h5>
--- readme.md
+++ readme.md
@@ -3,3 +3,10 @@
 This is a simple WordPress plugin to use shortcodes for Bootstrap 4 elements.
 
 This won't include all Bootstrap elements. For example, it won't include the navbar. However, more shortcodes will be added... eventually.
+
+
+## Included Shortcodes
+
+**Card**
+
+`[card title="test title" img="img.jpg" alt="Image Alt"]test text here[/card]`
