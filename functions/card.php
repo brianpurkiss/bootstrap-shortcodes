b16c75f70a60b13df9d5171174a7b6a9f66a96a6
Brian Purkiss <brianpurkiss@users.noreply.github.com>
2017-11-29 07:25:12 -0600

    Removed rogue lorem ipsum

--- functions/card.php
+++ functions/card.php
@@ -28,7 +28,6 @@ function bss_bootstrap_card( $atts, $content = null ) {
 				<img class="card-img-top" src="' . esc_attr($img) . '" alt="' . esc_attr($alt) . '">
 				<div class="card-block">
 					<h5 class="card-title">' . esc_attr($title) . '</h5>
-					lorem ipsum
 					<p class="card-text">' . esc_attr($content) . '</p>
 				</div>
 			</div>
