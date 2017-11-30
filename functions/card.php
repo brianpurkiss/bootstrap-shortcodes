90e32a9ce4330fd51dcb1c8400c7f38746e6fc81
Brian Purkiss <brianpurkiss@users.noreply.github.com>
2017-11-30 10:08:35 -0600

    Getting rid of the <h5>

--- functions/card.php
+++ functions/card.php
@@ -27,7 +27,6 @@ function bss_bootstrap_card( $atts, $content = null ) {
 			<div class="card" style="max-width:' . esc_attr($width) . 'px;float:' . esc_attr($float) . '">
 				<img class="card-img-top" src="' . esc_attr($img) . '" alt="' . esc_attr($alt) . '">
 				<div class="card-block">
-					<h5 class="card-title">' . esc_attr($title) . '</h5>
 					<p class="card-text">' . esc_attr($content) . '</p>
 				</div>
 			</div>
